b078224f40e4e544a8fd6620ff76861195ecfb8c
Kuba Kwiatkowski <jakub.kwiatkowski1981@gmail.com>
2021-10-03 14:51:43 +0200

    added: - I'll get the index sent in the address '/: index' with - I need to check if the answer is correct but first I need to download a CURRENT question!        - however, it will not be const nextQuestion = questions [goodAnswers - 1]; because if I'm at the question with index zero - this is the next question that is my topical question! I change the name from the next question to the current question that suggests to me, i.e. 'question' - I am sending a reply, if the correct answer button has been clicked I am sending true, otherwise false

--- routes/game.js
+++ routes/game.js
@@ -7,14 +7,14 @@ let halfOnHalfUsed = false;
 
 const questions = [
     {
-        question: 'Czy ten kurs jest fajny? ',
-        answers: ['Nie wiem', 'Oczywiście, że tak', 'Nie', 'Jest najlepszy!'],
-        correctAnswer: 3,
-    },
-    {
         question: 'jaki jest najlepszy jezyk programowania wg. mnie? ',
         answers: ['C++', 'Fortran', 'JavaScript', 'Java'],
         correctAnswer: 2,
+    },
+    {
+        question: 'Czy ten kurs jest fajny? ',
+        answers: ['Nie wiem', 'Oczywiście, że tak', 'Nie', 'Jest najlepszy!'],
+        correctAnswer: 3,
     },
     {
         question: 'Czy chcesz zjeść pizze? ',
@@ -45,6 +45,41 @@ app.get('/question', (req, res) => {
         })
     }
 });
+
+
+app.post('/answer/:index', (req, res) => {
+
+// pobiedam index przeslany w adresie '/:index' za pomocą req.params
+    const {index} = req.params;
+// Muszę sprawdzić czy odpowiedź jest prawidłowa ale najpierw muszę pobrać  AKTUALNE pytanie!
+// nie będzie to jednak  const nextQuestion = questions[goodAnswers - 1]; bo jesli jestem przy pytaniu z index'em zero
+// to właśnie następne pytanie jest moim pytaniem aktualnym! Zmieniam nazwę z pytania nastepnego na sugerujące
+// mi aktualne pytanie czyli 'question'
+    const question = questions[goodAnswers];
+    const {correctAnswer} = question;
+// Wysyłam odpowiedź, jeśli kliknięto przycisk poprawnej odpowiedzi wysyłam true w innym wypadku false
+
+    res.json({
+        correct: correctAnswer === Number(index), // Najkrótsza wersja zapisów pod spodem
+    });
+
+    // res.json({
+    //     correct: correctAnswer === Number(index)? true : false,
+    // });
+
+    // if (correctAnswer === Number(index)) {
+    //     res.json({
+    //         correct: true,
+    //     });
+    // } else {
+    //     res.json({
+    //         correct: false,
+    //     });
+    // }
+
+});
+
+
 
 }
 

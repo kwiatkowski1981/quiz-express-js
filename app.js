dd29e2aca81141ed3386e39efeb2df84fd7ecd4f
Kuba Kwiatkowski <jakub.kwiatkowski1981@gmail.com>
2021-10-03 12:47:15 +0200

    added goodAnswers counter, helps and questions to my backend

--- app.js
+++ app.js
@@ -6,3 +6,27 @@ const myUrl = '127.0.0.1';
 app.listen(port, myUrl, () => {
     console.log(`Serwer dziala na http://${myUrl}:${port}`);
 });
+
+
+let goodAnswers = 0;
+let callToAFriendUsed = false;
+let questionToTheCrowdUsed = false;
+let halfOnHalfUsed = false;
+
+const questions = [
+    {
+        question: 'Czy ten kurs jest fajny? ? ',
+        answers: ['Nie wiem', 'Oczywiście, że tak', 'Nie', 'Jest najlepszy!'],
+        correctAnswer: 3,
+    },
+    {
+        question: 'jaki jest najlepszy jezyk programowania wg. mnie? ',
+        answers: ['C++', 'Fortran', 'JavaScript', 'Java'],
+        correctAnswer: 2,
+    },
+    {
+        question: 'Czy chcesz zjeść pizze? ',
+        answers: ['Nawet dwie!', 'Jestem na diecie.', 'Nie, dziękuję', 'Wolę brokuły'],
+        correctAnswer: 0,
+    },
+];
